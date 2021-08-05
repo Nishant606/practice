ecb4bef097f17346793c8be9fe4ca3a04e8aab76
Nishant606 <patilnishant222@gmail.com>
2021-08-05 14:17:44 +0530

    added audio to eventlistener

--- drumkit/index.js
+++ drumkit/index.js
@@ -2,7 +2,7 @@ var totalDrumButtons=document.querySelectorAll(".drum").length;
 for(var i=0;i<totalDrumButtons;i++) {
 
 document.querySelectorAll(".drum")[i].addEventListener("click",function() {
-  var audio=new Audio("crash.mp3");
+  var audio=new Audio("sounds/crash.mp3");
   audio.play();
 });
 }
