00f55dc250aee1ae61aa45139064b6c0ce209a56
Nishant606 <patilnishant222@gmail.com>
2021-08-11 11:54:12 +0530

    adding functionalities

--- simon/game.js
+++ simon/game.js
@@ -1,7 +1,11 @@
+var gamePattern=[];
 var buttonColours=["red","blue","green","yellow"];
 function nextSequence(){
     var n=Math.random();
     n=n*4;
     var randomNumber=Math.floor(n);
-    return randomNumber;
+var randomChosenColour=buttonColours[randomNumber];
+gamePattern.push(randomChosenColour);
+    return gamePattern;
 }
+alert(nextSequence());
